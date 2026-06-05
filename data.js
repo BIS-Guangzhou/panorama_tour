7f7e24ad11b6c94908ade99214df8e665c3dcb5e
DavidWiehls <david.wiehls@rwth-aachen.de>
2026-06-05 19:55:14 +0800

    new pictures from pocket

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-third-floor",
-      "name": "Third Floor",
+      "id": "0-sports-field",
+      "name": "Sports Field",
       "levels": [
         {
           "tileSize": 256,
@@ -30,255 +30,22 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.446060567913668,
-          "pitch": 0.07317408579475426,
+          "yaw": -0.6038775766704685,
+          "pitch": -0.02289024703665632,
           "rotation": 0,
-          "target": "1-second-floor"
+          "target": "1-basketball-court"
+        },
+        {
+          "yaw": -2.4642499640131597,
+          "pitch": 0.11880522574613295,
+          "rotation": 0,
+          "target": "4-1f---"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-second-floor",
-      "name": "Second Floor",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 0.951490231851146,
-        "pitch": 0,
-        "fov": 1.325599857056214
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.16044612829101546,
-          "pitch": 0.07993574047189611,
-          "rotation": 0,
-          "target": "0-third-floor"
-        },
-        {
-          "yaw": 0.3833463292510899,
-          "pitch": 0.15597524626665304,
-          "rotation": 3.141592653589793,
-          "target": "3-first-floor"
-        },
-        {
-          "yaw": 0.9197169598406738,
-          "pitch": 0.24276646968733928,
-          "rotation": 5.497787143782138,
-          "target": "7-steam-classroom"
-        },
-        {
-          "yaw": 1.403663475903297,
-          "pitch": 0.10358090803112674,
-          "rotation": 0,
-          "target": "8-arts-classroom"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-exhibition-hall-1f",
-      "name": "Exhibition Hall (1F)",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.9359886313007397,
-          "pitch": 0.2913936290447854,
-          "rotation": 0,
-          "target": "1-second-floor"
-        },
-        {
-          "yaw": -2.1165899374767907,
-          "pitch": 0.258081156803021,
-          "rotation": 0,
-          "target": "3-first-floor"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-first-floor",
-      "name": "First Floor",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.4197362049887907,
-          "pitch": 0.07889490760959816,
-          "rotation": 0,
-          "target": "2-exhibition-hall-1f"
-        },
-        {
-          "yaw": 0.1570117653754579,
-          "pitch": -0.020867989082905325,
-          "rotation": 0.7853981633974483,
-          "target": "4-admissions"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-admissions",
-      "name": "Admissions",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.4483514133272397,
-          "pitch": 0.02548894106895716,
-          "rotation": 0,
-          "target": "3-first-floor"
-        },
-        {
-          "yaw": -2.591121853421358,
-          "pitch": 0.07574984346738312,
-          "rotation": 0,
-          "target": "5-student-entrance"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": 1.6748164356097135,
-          "pitch": 0.27731439117395773,
-          "title": "Student Service",
-          "text": "At Student Services, we support your child’s academic, emotional, and social growth while guiding their path to higher education, ensuring they thrive and feel right at home in our diverse global community."
-        }
-      ]
-    },
-    {
-      "id": "5-student-entrance",
-      "name": "Student Entrance",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.2196866557641677,
-          "pitch": 0.013317950818576563,
-          "rotation": 0,
-          "target": "4-admissions"
-        },
-        {
-          "yaw": -1.304557797940097,
-          "pitch": -0.024151289797426756,
-          "rotation": 0,
-          "target": "6-basketball-court"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "6-basketball-court",
+      "id": "1-basketball-court",
       "name": "Basketball Court",
       "levels": [
         {
@@ -301,23 +68,180 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -1.4715166784794977,
         "pitch": 0,
-        "fov": 1.325599857056214
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": 0.6435235388358649,
-          "pitch": 0.041424866750480405,
+          "yaw": -1.4640734197060326,
+          "pitch": 0.03970388992365059,
           "rotation": 0,
-          "target": "5-student-entrance"
+          "target": "0-sports-field"
+        },
+        {
+          "yaw": 0.8242699274730967,
+          "pitch": 0.12296847714109305,
+          "rotation": 5.497787143782138,
+          "target": "2-1f"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "7-steam-classroom",
-      "name": "STEAM Classroom",
+      "id": "2-1f",
+      "name": "1F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2023.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9048131767307961,
+          "pitch": 0.06783197293155752,
+          "rotation": 0.7853981633974483,
+          "target": "4-1f---"
+        },
+        {
+          "yaw": 0.31963658837905307,
+          "pitch": 0.042450839727489864,
+          "rotation": 5.497787143782138,
+          "target": "3-1f--"
+        },
+        {
+          "yaw": 3.1111544901886283,
+          "pitch": 0.12287129827238807,
+          "rotation": 13.351768777756625,
+          "target": "1-basketball-court"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.202429508602778,
+          "pitch": -0.1881455057517183,
+          "title": "Students Service",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-1f--",
+      "name": "1F -",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2010.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4687804182608062,
+          "pitch": 0.111554376530691,
+          "rotation": 5.497787143782138,
+          "target": "4-1f---"
+        },
+        {
+          "yaw": 3.082036814189382,
+          "pitch": 0.09779818154043518,
+          "rotation": 0.7853981633974483,
+          "target": "2-1f"
+        },
+        {
+          "yaw": 0.7925340367340965,
+          "pitch": 0.23370019393952646,
+          "rotation": 0.7853981633974483,
+          "target": "5-2f"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-1f---",
+      "name": "1F --",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2019.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3876519910041907,
+          "pitch": 0.14236427359463377,
+          "rotation": 6.283185307179586,
+          "target": "0-sports-field"
+        },
+        {
+          "yaw": -1.7870679080126841,
+          "pitch": 0.21535091154746766,
+          "rotation": 5.497787143782138,
+          "target": "3-1f--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-2f",
+      "name": "2F",
       "levels": [
         {
           "tileSize": 256,
@@ -345,17 +269,91 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.171559734028744,
-          "pitch": -0.0937390703991774,
+          "yaw": -0.7418741789574383,
+          "pitch": 0.25273241647436784,
           "rotation": 0,
-          "target": "1-second-floor"
+          "target": "6-2f---artsexhibition"
+        },
+        {
+          "yaw": -1.734367889797646,
+          "pitch": 0.10139620656288528,
+          "rotation": 0,
+          "target": "10-library"
+        },
+        {
+          "yaw": 0.9413113963417619,
+          "pitch": 0.11954351502674321,
+          "rotation": 5.497787143782138,
+          "target": "8-2f---arts-1"
+        },
+        {
+          "yaw": 2.7361467128355965,
+          "pitch": 0.32125154488299756,
+          "rotation": 9.42477796076938,
+          "target": "3-1f--"
+        },
+        {
+          "yaw": 2.484564768997892,
+          "pitch": 0.11909517301796946,
+          "rotation": 0,
+          "target": "11-3f"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "8-arts-classroom",
-      "name": "Arts Classroom",
+      "id": "6-2f---artsexhibition",
+      "name": "2F - Artsexhibition",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2013,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6685883205242158,
+          "pitch": 0.1998107920894796,
+          "rotation": 0.7853981633974483,
+          "target": "7-2f---artsexhibition-exit"
+        },
+        {
+          "yaw": -2.801234783047267,
+          "pitch": 0.280525590502986,
+          "rotation": 0.7853981633974483,
+          "target": "5-2f"
+        },
+        {
+          "yaw": -0.8067464714249333,
+          "pitch": 0.17400351461608032,
+          "rotation": 0,
+          "target": "10-library"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-2f---artsexhibition-exit",
+      "name": "2F - Artsexhibition Exit",
       "levels": [
         {
           "tileSize": 256,
@@ -383,20 +381,246 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.39274206659244904,
-          "pitch": 0.024951376656940027,
+          "yaw": -0.8163754755624524,
+          "pitch": 0.07311887393689709,
           "rotation": 0,
-          "target": "1-second-floor"
+          "target": "9-2f---arts-2"
+        },
+        {
+          "yaw": 2.502372190721191,
+          "pitch": 0.18660359923955738,
+          "rotation": 0,
+          "target": "10-library"
+        },
+        {
+          "yaw": 1.2437445465626311,
+          "pitch": 0.2763404359290611,
+          "rotation": 7.0685834705770345,
+          "target": "6-2f---artsexhibition"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-2f---arts-1",
+      "name": "2F - Arts 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2009,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.177255723743265,
+          "pitch": 0.1523286768657499,
+          "rotation": 0,
+          "target": "9-2f---arts-2"
+        },
+        {
+          "yaw": 2.1079094809474013,
+          "pitch": 0.19842406659870981,
+          "rotation": 0.7853981633974483,
+          "target": "5-2f"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-2f---arts-2",
+      "name": "2F - Arts 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1246321097526497,
+          "pitch": 0.08529523359908886,
+          "rotation": 5.497787143782138,
+          "target": "7-2f---artsexhibition-exit"
+        },
+        {
+          "yaw": -0.6822530748202986,
+          "pitch": 0.1030304321966824,
+          "rotation": 6.283185307179586,
+          "target": "8-2f---arts-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-library",
+      "name": "Library",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2760338078204647,
+          "pitch": 0.43050119127354947,
+          "rotation": 0,
+          "target": "7-2f---artsexhibition-exit"
+        },
+        {
+          "yaw": -1.1431608242895859,
+          "pitch": 0.0222925167139163,
+          "rotation": 4.71238898038469,
+          "target": "5-2f"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-3f",
+      "name": "3F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2036,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0250502189342843,
+          "pitch": 0.21142547278636847,
+          "rotation": 3.141592653589793,
+          "target": "5-2f"
+        },
+        {
+          "yaw": -2.269630523536918,
+          "pitch": -0.021201718361869126,
+          "rotation": 0,
+          "target": "12-4f"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-4f",
+      "name": "4F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7082076209833517,
+          "pitch": 0.13767928513347272,
+          "rotation": 3.141592653589793,
+          "target": "11-3f"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "Britannia International School of Guangzhou",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": true
+    "viewControlButtons": false
   }
 };
